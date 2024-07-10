4c14b66c3f7b3e0a8cf482962eeeff900e0e7bc7
gfantoni <guilhermefantoniborges@gmail.com>
2024-07-10 15:48:41 -0300

    Makefile, source and include dir added to ex01

---
+++ cpp00/ex01/source/main.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/10 11:21:45 by gfantoni          #+#    #+#             */
+/*   Updated: 2024/07/10 15:44:07 by gfantoni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Contacts.hpp"
+
+std::string options(void);
+
+int main(void)
+{
+	Contact someone;
+	
+	// std::string userinput;
+	// userinput = options();
+	// std::cout << userinput << std::endl;
+
+	std::cout << someone.getFirstName() << std::endl;
+	return (0);
+}
+
+std::string options(void)
+{
+	std::string userinput;
+	while (userinput != "ADD" && userinput != "SEARC" && userinput != "EXIT")
+	{
+		std::cout << std::endl << "-----menu-----" << std::endl << std::endl;
+		std::cout << "ADD" << std::endl;
+		std::cout << "SEARCH" << std::endl;
+		std::cout << "EXIT" << std::endl << std::endl;
+		std::cout << "> ";
+		std::getline(std::cin, userinput);
+	}
+	return (userinput);
+}

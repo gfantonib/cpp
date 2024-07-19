9547a4671e532793d30333111ec022f2beeedfd5
gfantoni <guilhermefantoniborges@gmail.com>
2024-07-19 12:11:11 -0300

    s1 is beeing erased and replaced by s2

--- cpp01/ex04/source/main.cpp
+++ cpp01/ex04/source/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 11:21:45 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/19 11:55:29 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/19 12:07:34 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 {
 	if (argc != 4)
 	{
-		std::cout << "wrong argument count!\nusage: ./loser filename s1 s2" << std::endl;
+		std::cout << "wrong argument count!\nusage: ./executable filename s1 s2" << std::endl;
 		return (1);
 	}
 	std::ifstream src_file(argv[1]);
@@ -29,13 +29,16 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	std::string s1(argv[2]);
-	std::size_t len = s1.length();
+	std::string s2(argv[3]);
+	std::size_t s1_len = s1.length();
+	// std::size_t s2_len = s2.length();
 	std::string line;
 	std::size_t start;
 	while (std::getline(src_file, line))
 	{
 		start = line.find(s1);
-		line.erase(start, len);
+		line.erase(start, s1_len);
+		line.insert(start, s2);
 		std::cout << line << std::endl;
 	}
 	src_file.close();

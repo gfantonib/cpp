f56ed8dac0885bedd3951a8ee402da3b64a99c5b
gfantoni <guilhermefantoniborges@gmail.com>
2024-07-19 13:07:19 -0300

    new file is being created and properly filled.

--- cpp01/ex04/source/main.cpp
+++ cpp01/ex04/source/main.cpp
@@ -6,14 +6,12 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 11:21:45 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/19 12:07:34 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/19 13:03:44 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
-// #include <sstream>
-// #include <string>
 
 int main(int argc, char *argv[])
 {
@@ -23,15 +21,19 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	std::ifstream src_file(argv[1]);
+	std::string out_filename(argv[1]);
+	out_filename = out_filename + ".replace";
+	std::ofstream out_file(out_filename.c_str());
 	if (!src_file.is_open())
 	{
 		std::cout << "src_file could not be opened!" << std::endl;
+		src_file.close();
+		out_file.close();
 		return (1);
 	}
 	std::string s1(argv[2]);
 	std::string s2(argv[3]);
 	std::size_t s1_len = s1.length();
-	// std::size_t s2_len = s2.length();
 	std::string line;
 	std::size_t start;
 	while (std::getline(src_file, line))
@@ -39,8 +41,9 @@ int main(int argc, char *argv[])
 		start = line.find(s1);
 		line.erase(start, s1_len);
 		line.insert(start, s2);
-		std::cout << line << std::endl;
+		out_file << line << std::endl;
 	}
 	src_file.close();
+	out_file.close();
 	return (0);
 }

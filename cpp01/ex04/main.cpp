66404cda0ce990414428e414ca4fba35c3cd4e14
gfantoni <guilhermefantoniborges@gmail.com>
2024-07-19 17:23:17 -0300

    protections added to main.cpp

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 11:21:45 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/19 13:03:44 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/19 17:20:41 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,11 @@ int main(int argc, char *argv[])
 		out_file.close();
 		return (1);
 	}
+	if (src_file.peek() == std::ifstream::traits_type::eof())
+	{
+		std::cout << "empty file! aborting..." << std::endl;
+		return (1);
+	}
 	std::string s1(argv[2]);
 	std::string s2(argv[3]);
 	std::size_t s1_len = s1.length();
@@ -39,6 +44,8 @@ int main(int argc, char *argv[])
 	while (std::getline(src_file, line))
 	{
 		start = line.find(s1);
+		if (start == std::string::npos)
+			continue;
 		line.erase(start, s1_len);
 		line.insert(start, s2);
 		out_file << line << std::endl;

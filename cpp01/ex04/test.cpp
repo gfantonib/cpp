b75f6a2204ddb5b839ffa77cdeba245dadb4e0a6
gfantoni <guilhermefantoniborges@gmail.com>
2024-07-19 17:24:56 -0300

    creating files and testing them in test.cpp

--- cpp01/ex04/test.cpp
+++ cpp01/ex04/test.cpp
@@ -6,17 +6,26 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 14:19:59 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/19 14:25:10 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/19 16:43:44 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 
 int main(void)
 {
-	std::string command("./executable file xxx yyy");
-	int result = system(command.c_str());
-	std::cout << result << std::endl;
+	std::ofstream file1("file1");
+	file1 << "xxxaaaaaa" << std::endl;
+	file1 << "aaaxxxaaa" << std::endl;
+	file1 << "aaaaaaxxx" << std::endl;
+	std::string command("./executable file1 xxx yyy");
+	system(command.c_str());
+
+	std::ofstream file2("file2");
+	file2 << "" << std::endl;
+	command = "./executable file2 xxx yyy";
+	system(command.c_str());
 	return (0);
 }

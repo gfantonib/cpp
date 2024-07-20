a5205c136b70e467c1ebd2282d823fea96e511f7
gfantoni <guilhermefantoniborges@gmail.com>
2024-07-20 10:08:17 -0300

    more than one pattern per line test added and program aproved

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,12 +6,14 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 11:21:45 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/20 09:10:35 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/20 10:07:00 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+
+void ReplacePattern(std::ifstream &src_file, std::ofstream &out_file, std::string s1, std::string s2);
 
 int main(int argc, char *argv[])
 {
@@ -47,22 +49,26 @@ int main(int argc, char *argv[])
 		out_file.close();
 		return (1);
 	}
+	ReplacePattern(src_file, out_file, s1, s2);
+	src_file.close();
+	out_file.close();
+	return (0);
+}
+
+void ReplacePattern(std::ifstream &src_file, std::ofstream &out_file, std::string s1, std::string s2)
+{
 	std::size_t s1_len = s1.length();
 	std::string line;
 	std::size_t start;
 	while (std::getline(src_file, line))
 	{
 		start = line.find(s1);
-		if (start == std::string::npos)
+		while (start != std::string::npos)
 		{
-			out_file << line << std::endl;
-			continue;
+			line.erase(start, s1_len);
+			line.insert(start, s2);
+			start = line.find(s1);
 		}
-		line.erase(start, s1_len);
-		line.insert(start, s2);
 		out_file << line << std::endl;
 	}
-	src_file.close();
-	out_file.close();
-	return (0);
 }
--- cpp01/ex04/test.cpp
+++ cpp01/ex04/test.cpp
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 14:19:59 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/20 09:08:00 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/20 09:27:16 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,13 @@ int main(void)
 	file7 << "aaaaaaaaa" << std::endl;
 	file7 << "aaaxxxaaa" << std::endl;
 	command = "./executable file7 xxx yyy";
+	system(command.c_str());
+
+	std::ofstream file8("file8");
+	file8 << "xxxaaaxxx" << std::endl;
+	file8 << "aaaxxxaaa" << std::endl;
+	file8 << "xxxaaaxxx" << std::endl;
+	command = "./executable file8 xxx yyy";
 	system(command.c_str());
 	return (0);
 }

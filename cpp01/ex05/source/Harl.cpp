248e8b2bbc9f890036ca6abfb02f74026bc3fdbb
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-07-24 15:06:49 -0300

    messages improved

--- cpp01/ex05/source/Harl.cpp
+++ cpp01/ex05/source/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 11:26:19 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/24 14:49:52 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/24 15:04:50 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 void Harl::debug( void )
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::cout << "Debug message!" << std::endl;
 }
 
 void Harl::info( void )
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+	std::cout << "Info message!" << std::endl;
 }
 
 void Harl::warning( void )
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "Warning message!" << std::endl;
 }
 
 void Harl::error( void )
 {
-	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "Error message!" << std::endl;
 }
 
 int Harl::test( int nbr )
@@ -41,14 +41,6 @@ void Harl::complain( std::string level )
 {
 	typedef void (Harl::*poiter_to_function)( void );
 	std::map <std::string, poiter_to_function> functionMap;
-	
-	// functionMap = 
-	// {
-	// 	{"debug", &Harl::debug},	
-	// 	{"info", &Harl::info},	
-	// 	{"warning", &Harl::warning},	
-	// 	{"error", &Harl::error}
-	// };
 
 	functionMap["debug"] = &Harl::debug;
 	functionMap["info"] = &Harl::info;

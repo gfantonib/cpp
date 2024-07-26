98d6bd3cf820337f2ad951460367ad426798a340
gfantoni <guilhermefantoniborges@gmail.com>
2024-07-26 11:04:41 -0300

    copy assignment operator is now using the getRawBits member function

--- cpp02/ex00/source/Fixed.cpp
+++ cpp02/ex00/source/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 11:26:19 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/26 10:54:01 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/26 11:03:14 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,20 @@ Fixed::Fixed( void )
 	std::cout << "Default constructor called" << std::endl;
 	this->fixed_point_nbr = 0;
 }
+
 Fixed::Fixed( const Fixed &to_be_copied )
 {
 	std::cout << "Copy constructor called" << std::endl;
 	
 	*this = to_be_copied;
 }
-Fixed& Fixed::operator = ( const Fixed &to_be_copied )
+
+Fixed& Fixed::operator = ( const Fixed &to_be_copied ) // c = b
 {
 	std::cout << "Copy assignment operator called" << std::endl;
 	
 	if (this != &to_be_copied)
-		this->fixed_point_nbr = (&to_be_copied)->fixed_point_nbr;
+		this->fixed_point_nbr = to_be_copied.getRawBits();
 	return *this;
 }
 
@@ -36,12 +38,12 @@ Fixed::~Fixed( void )
 {
 	std::cout << "Destructor called" << std::endl;
 }
-int Fixed::getRawBits( void ) const // the function is a "const member function" and promises not to change the state of the object.
+
+int Fixed::getRawBits( void ) const // the function is a "const member function" and promises not to change the state of the object
 {
 	std::cout << "getRawBits member function called" << std::endl;
 
 	return this->fixed_point_nbr;
-	
 }
 
 void Fixed::setRawBits( int const raw )

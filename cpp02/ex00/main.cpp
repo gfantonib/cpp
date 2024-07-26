75f78f6bd59e335aebbe0fa7672dacae70cd2518
gfantoni <guilhermefantoniborges@gmail.com>
2024-07-26 10:58:10 -0300

    cpp02 ex00 implemented and is printing the output equal to the subject.

---
+++ cpp02/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/10 11:21:45 by gfantoni          #+#    #+#             */
+/*   Updated: 2024/07/26 10:54:51 by gfantoni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void ) 
+{
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+
+	c = b;
+	
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	
+	return 0;
+}

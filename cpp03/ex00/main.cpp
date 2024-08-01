a2d1ebc76970ebee551baa8d98d69c4b41f2982a
gfantoni <guilhermefantoniborges@gmail.com>
2024-08-01 15:14:38 -0300

    test implemented.

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 11:21:45 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/08/01 14:52:27 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/08/01 15:11:56 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,12 @@ int main( void )
 
 	blue.attack("red");
 	red.takeDamage(3);
-	blue.attack("red");
-	red.takeDamage(3);
-	blue.attack("red");
-	red.takeDamage(3);
 
-	red.beRepaired(3);
-	red.beRepaired(3);
-	red.beRepaired(3);
-	red.beRepaired(3);
+	red.attack("blue");
+	blue.takeDamage(2);
 	
-	red.attack("blue");
-	red.attack("blue");
+	blue.beRepaired(1);
+	red.beRepaired(2);
 
 	red.showPoints();
 	blue.showPoints();

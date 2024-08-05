d3d210b023d523487674890ccf558be34bc4bab2
gfantoni <guilhermefantoniborges@gmail.com>
2024-08-05 11:37:17 -0300

    main test with important part commented.

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,17 +6,34 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 11:21:45 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/08/05 10:18:18 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/08/05 11:36:33 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Brain.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
 
 int main( void )
 {
-	Cat cat;
-	cat.setIdeas("fish");
-	cat.showIdeas();
+	Cat a;
+	a.setIdeas("fish");
+	a.showIdeas();
+	
+	Cat b;
+	b.showIdeas();
+	b = a;
+	b.showIdeas();
+	
+	
+	// Animal* animals[100];
+	// for (int i = 0; i < 100; i++)
+	// {
+	// 	if (i < 50)
+	// 		animals[i] = new Dog;
+	// 	else
+	// 		animals[i] = new Cat;
+	// }
+	// animals[1]->getType();
+	// for (int i = 0; i < 100; i++)
+	// 	delete animals[i];
 }

78b64f3827a8c0be4e7f386ff662d736231caab8
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-08-17 16:05:50 -0300

    comment about pure virtual function improved.

--- cpp04/ex02/include/Animal.hpp
+++ cpp04/ex02/include/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 11:23:23 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/08/07 15:28:25 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/08/17 15:30:53 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ class Animal
 	virtual ~Animal();
 
 	// Member functions
-	virtual void makeSound( void ) const = 0; // This is a pure virtual function and it prohibits for you to declare a Anmial type.
+	virtual void makeSound( void ) const = 0; // This is a pure virtual function and it prohibits for you to declare (instantiate) a Anmial type.
 	void setType( std::string str );
 	std::string getType( void ) const;
 };
